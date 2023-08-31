e0ee43ed10b2195e6d2c2b4d17157358930703c1
afshankhan <afshan97khan@gmail.com>
2023-08-31 18:19:14 +0530

    Implementing Redis session management for improved scalability and user experience

--- src/main/java/com/example/bloggingapp/BloggingAppApplication.java
+++ src/main/java/com/example/bloggingapp/BloggingAppApplication.java
@@ -2,14 +2,10 @@ package com.example.bloggingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class BloggingAppApplication {
 
 
