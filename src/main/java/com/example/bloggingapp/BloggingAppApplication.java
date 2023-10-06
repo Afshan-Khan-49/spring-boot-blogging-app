5668b09cc9aac6ff24a5de3cce386d2b2ff99304
afshankhan <afshan97khan@gmail.com>
2023-10-06 20:49:27 +0530

    Updating redis config

--- src/main/java/com/example/bloggingapp/BloggingAppApplication.java
+++ src/main/java/com/example/bloggingapp/BloggingAppApplication.java
@@ -2,7 +2,9 @@ package com.example.bloggingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
@@ -15,5 +17,9 @@ public class BloggingAppApplication {
         SpringApplication.run(BloggingAppApplication.class, args);
     }
 
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 
 }
